77761a07e3b9cdba36e24086c79c6f5716296eb2
Leyla Mehtiyeva <leylamehtiyevaa@gmail.com>
2023-07-09 20:21:13 +0400

    Basics of Angular

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -45,9 +45,11 @@ namespace API.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200/");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
             });
+
+
             return services;
         }
     }
