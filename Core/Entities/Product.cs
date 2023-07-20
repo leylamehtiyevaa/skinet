7df075b283fc15f9a49487fa2eaabc3dd7d5aa4d
Leyla Mehtiyeva <leylamehtiyevaa@gmail.com>
2023-07-20 14:36:56 +0400

    Error Handling(client)

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -5,7 +5,6 @@ namespace Core.Entities
         public string Name {get; set; }
         public string Description {get; set; }
         public decimal Price {get; set; }
-       // public string Picture {get; set; }
         public string PictureUrl {get; set; }
         public ProductType ProductType {get; set; }
         public int ProductTypeId {get; set; }
